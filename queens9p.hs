e6f3ac47e2629c0c061eb6c0ef4d782483a26ae2
sasamil <sasa.milenkovic.xyz@gmail.com>
2016-12-26 20:51:26 +0100

    Update queens9p.hs

--- queens9p.hs
+++ queens9p.hs
@@ -1,6 +1,6 @@
 -- The raw solution for the derived problem of placing 1 pown and 9 queens on a 8X8 chessboard so that no two queens 
--- threaten each other. There are 16 independant solutions and 128 dependent solutions. This file also contain a set 
--- of set of auxiliary functions for detecting and filtering out the dependent solutions.
+-- threaten each other. There are 16 independant solutions (queens9pind) and 128 dependent solutions (queens9p). 
+-- This file also contain a set of set of auxiliary functions for detecting and filtering out the dependent solutions.
 
 import Data.List 
 
@@ -40,6 +40,7 @@ unrelated3 func p y (x:xs) =  if func p y x then False else unrelated3 func p y 
 
 
 queens9p = nub queens
+queens9pind = filterout $ nub queensi
 
 -------------------------------------------------------
 queens = [((x,y), sort [(x1,y1),(x2,y2),(x3,y3),(x4,y4), (x5,y5),(x6,y6),(x7,y7),(x8,y8),(x9,y9)]) | 
@@ -76,6 +77,43 @@ queens = [((x,y), sort [(x1,y1),(x2,y2),(x3,y3),(x4,y4), (x5,y5),(x6,y6),(x7,y7)
   unrelated2 diag_related2 (x9,y9) [(x1,y1),(x2,y2),(x3,y3),(x4,y4)],
   unrelated3 diag_related3 (x,y) (x9,y9) [(x5,y5),(x6,y6),(x7,y7),(x8,y8)]
          ]
+          
+-------------------------------------------------------
+queensi = [((x,y), sort [(x1,y1),(x2,y2),(x3,y3),(x4,y4), (x5,y5),(x6,y6),(x7,y7),(x8,y8),(x9,y9)]) | 
+  x  <- [2..4], y  <- [2..4],
+   
+  x1 <- [x], y1 <- [1..y-1], 
+  x2 <- [x], y2 <- [y+1..8], 
+  
+  y3 <- [y], x3 <- [1..x-1], not $ diag_related2 (x3,y3) (x1,y1), not $ diag_related2 (x3,y3) (x2,y2),
+  y4 <- [y], x4 <- [x+1..8], not $ diag_related2 (x4,y4) (x1,y1), not $ diag_related2 (x4,y4) (x2,y2),
+
+  
+  x5 <- [1..8] \\ [x,x3,x4], 
+  y5 <- [1..8] \\ [y,y1,y2], 
+  unrelated2 diag_related2 (x5,y5) [(x1,y1),(x2,y2),(x3,y3),(x4,y4)],
+  
+  x6 <- [1..8] \\ [x,x3,x4, x5], 
+  y6 <- [1..8] \\ [y,y1,y2, y5], 
+  unrelated2 diag_related2 (x6,y6) [(x1,y1),(x2,y2),(x3,y3),(x4,y4)],
+  unrelated3 diag_related3 (x,y) (x6,y6) [(x5,y5)],
+  
+  x7 <- [1..8] \\ [x,x3,x4, x5,x6], 
+  y7 <- [1..8] \\ [y,y1,y2, y5,y6], 
+  unrelated2 diag_related2 (x7,y7) [(x1,y1),(x2,y2),(x3,y3),(x4,y4)],
+  unrelated3 diag_related3 (x,y) (x7,y7) [(x5,y5),(x6,y6)],
+  
+  x8 <- [1..8] \\ [x,x3,x4, x5,x6,x7], 
+  y8 <- [1..8] \\ [y,y1,y2, y5,y6,y7], 
+  unrelated2 diag_related2 (x8,y8) [(x1,y1),(x2,y2),(x3,y3),(x4,y4)],
+  unrelated3 diag_related3 (x,y) (x8,y8) [(x5,y5),(x6,y6),(x7,y7)],
+  
+  x9 <- [1..8] \\ [x,x3,x4, x5,x6,x7,x8], 
+  y9 <- [1..8] \\ [y,y1,y2, y5,y6,y7,y8], 
+  unrelated2 diag_related2 (x9,y9) [(x1,y1),(x2,y2),(x3,y3),(x4,y4)],
+  unrelated3 diag_related3 (x,y) (x9,y9) [(x5,y5),(x6,y6),(x7,y7),(x8,y8)]
+         ]
+          
           
           
 -- =====================================================
